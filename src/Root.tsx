5136aae16f4b261a615db694d09b775ccdedc9d3
InhaKr <inkr3891@gmail.com>
2026-02-20 19:36:51 +0200

    1

---
+++ src/Root.tsx
@@ -0,0 +1,26 @@
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { App } from './App';
+import { Home } from './components/Home';
+import { TabsPage } from './components/TabsPage';
+import { Navigate } from 'react-router-dom';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
---
+++ src/components/Nav.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export const Nav = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link
+            to="/"
+            className={`navbar-item ${pathname === '/' ? 'is-active' : ''}`}
+          >
+            Home
+          </Link>
+          <Link
+            to="/tabs"
+            className={`navbar-item ${pathname.startsWith('/tabs') ? 'is-active' : ''}`}
+          >
+            Tabs
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
